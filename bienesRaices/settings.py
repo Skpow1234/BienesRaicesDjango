190c1ba5eb8d7552839eb88cd68beb708f33516b
Juan Hurtado <jfhvjfhv0015@gmail.com>
2023-11-21 16:20:04 -0500

    Actualizamos dependencias

--- bienesRaices/settings.py
+++ bienesRaices/settings.py
@@ -77,7 +77,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bienesRaices/static')
@@ -92,4 +92,4 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
