138333092f081a94a556fdf34cdd34bb2b1e61dc
Juan Hurtado <jfhvjfhv0015@gmail.com>
2023-11-21 15:12:09 -0500

    Actualizamos dependencias

--- bienesRaices/settings.py
+++ bienesRaices/settings.py
@@ -75,7 +75,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATIC_ROOT= os.path.join(BASE_DIR, 'static', 'templates')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
